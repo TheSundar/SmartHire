c9cbfd72d13969f7e5024cfde48edb84ba9b3ca1
TheSundar <iamsundar04@gmail.com>
2016-07-24 12:20:13 +0530

    First check in

--- candidate/views.py
+++ candidate/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+#todo list client         any
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.generic import View
+
+class ClientView(View):
+    def get(self, request):
+
+        # <view logic>
+
+        return HttpResponse('result')
+
+
+
+
+#todo Add client         only manger and above
+#todo update client      only  manger and above
+#todo delete client      only  manger and above
+
+
+
+#todo list contact         any
+#todo Add contact         only manger and above
+#todo update contact      only  manger and above
+#todo delete contact      only  manger and above
+
+
+
+
+#
